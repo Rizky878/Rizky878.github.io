4c852396900fcc5076cddc055f4b4663812032a8
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2021-08-04 12:36:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ checkBtn.addEventListener("click", e =>{
   let inputVal = inputField.value.split('').join(' ');
   if(inputVal == captcha.innerText){ //if captcha matched
     statusTxt.style.color = "#4db2ec";
-    statusTxt.innerText = document.split('\n')
+    statusTxt.innerText = "Benar!"
     setTimeout(()=>{
       removeContent();
       getCaptcha();
