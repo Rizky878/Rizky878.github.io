34e18258e8588189494b5c68f5ffebf8808c100b
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2021-08-31 19:06:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ checkBtn.addEventListener("click", e =>{
   let inputVal = inputField.value.split('').join(' ');
   if(inputVal == captcha.innerText){ //if captcha matched
     statusTxt.style.color = "#4db2ec";
-    statusTxt.innerHTML = '';
+    statusTxt.href = 'docs';
     
   }else{
     statusTxt.style.color = "#ff0000";
