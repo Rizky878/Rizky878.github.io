ffde7742559d1f44be37d7bd53767d33c570ec00
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2021-08-31 19:02:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ checkBtn.addEventListener("click", e =>{
   let inputVal = inputField.value.split('').join(' ');
   if(inputVal == captcha.innerText){ //if captcha matched
     statusTxt.style.color = "#4db2ec";
-    statusTxt.innerHTML = "https://google.com"
+    statusTxt.innerHTML = ""
     setTimeout(()=>{
       removeContent();
       getCaptcha();
